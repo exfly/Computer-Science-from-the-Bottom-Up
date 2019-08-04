a1e4a5c02e23d29906e28c4d692ab167db195ab5
haifei.zhang <haifei.zhang@chaitin.com>
2019-08-04 17:49:31 +0800

    chapter7

---
+++ chapter7/compile-example/func.s
@@ -0,0 +1,33 @@
+	.section	__TEXT,__text,regular,pure_instructions
+	.build_version macos, 10, 14	sdk_version 10, 14
+	.globl	_function               ## -- Begin function function
+	.p2align	4, 0x90
+_function:                              ## @function
+	.cfi_startproc
+## %bb.0:
+	pushq	%rbp
+	.cfi_def_cfa_offset 16
+	.cfi_offset %rbp, -16
+	movq	%rsp, %rbp
+	.cfi_def_cfa_register %rbp
+	subq	$16, %rsp
+	movq	%rdi, -8(%rbp)
+	movq	-8(%rbp), %rsi
+	leaq	L_.str(%rip), %rdi
+	movb	$0, %al
+	callq	_printf
+	movq	_global@GOTPCREL(%rip), %rsi
+	movl	(%rsi), %ecx
+	movl	%eax, -12(%rbp)         ## 4-byte Spill
+	movl	%ecx, %eax
+	addq	$16, %rsp
+	popq	%rbp
+	retq
+	.cfi_endproc
+                                        ## -- End function
+	.section	__TEXT,__cstring,cstring_literals
+L_.str:                                 ## @.str
+	.asciz	"%s\n"
+
+
+.subsections_via_symbols
